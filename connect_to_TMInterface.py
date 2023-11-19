650c287790ce42beae99ab2dc5c9e6fdf3d79869
victrolles <95492416+victrolles@users.noreply.github.com>
2023-11-19 21:29:45 +0100

    clean code

--- connect_to_TMInterface.py
+++ connect_to_TMInterface.py
@@ -32,8 +32,7 @@ class MainClient(Client):
             current_state = self.agent.get_state(iface_state)
             
             # Print the current state of the car
-            # TODO: fix this fucking problem of acceleration
-            print(f"Spd: {current_state.speed}, Acc : {current_state.acceleration}", end='\r')
+            # print(f"Spd: {current_state.speed}, Acc : {current_state.acceleration}", end='\r')
             # print(f"Turn: {current_state.turning_rate:.1f}, Free: {current_state.is_free_wheeling}, Slide: {current_state.is_sliding}, Lat: {current_state.has_any_lateral_contact}",end='\r')
             # print(f"Dist: {current_state.distance_to_centerline:.3f}, Angle: {current_state.angle_to_centerline:.3f}",end='\r')
             # print(f"Dist1: {current_state.distance_to_first_turn:.3f}, Dist2: {current_state.distance_to_second_turn:.3f}, Dir1: {current_state.direction_of_first_turn:.3f}, Dir2: {current_state.direction_of_second_turn:.3f}", end='\r')
