1f57b820a605ccfa9a0779d4fa805ca0cdd9fc5a
Elise Albrecht <118198011+Vitally05@users.noreply.github.com>
2025-03-24 17:59:26 -0400

    last_flow

--- pytorch-pwc-sniklaus/run_multi.py
+++ pytorch-pwc-sniklaus/run_multi.py
@@ -1,17 +1,23 @@
 import subprocess
 import os
 
-interval = []       # [start, end] if empty, all files in images directory will be used
-file = "images_firstP"
+interval = []       # [start, end] if empty, all files in image directory will be used
+image_directory = "images_firstP"
+image_name = "ezgif-frame-"
 
 if __name__ == "__main__":
     input_file = []
 
     if not interval:
-        input_file = os.listdir(file)
+        input_file = os.listdir(image_directory)
     else :
         for i in range(interval[0], interval[1]+1):
-            input_file.append(f"frames/frame_000{i}.png")
+            if i < 10:
+                input_file.append(f"{image_directory}/{image_name}00{i}.png")
+            elif i < 100:
+                input_file.append(f"{image_directory}/{image_name}0{i}.png")
+            else:
+                input_file.append(f"{image_directory}/{image_name}{i}.png")
 
     if len(input_file) < 2:
         print("Not enough images to create flows")
@@ -19,9 +25,9 @@ if __name__ == "__main__":
 
     for i in range(len(input_file) - 1):
         # generate flow between two images
-        print(f"Creating flow between {file}/{input_file[i]} and {file}/{input_file[i+1]}")
+        print(f"Creating flow between {image_directory}/{input_file[i]} and {image_directory}/{input_file[i+1]}")
         result_flo = subprocess.run(
-            ["python", "generate_out_flow.py", "--model", "default", "--one", f"{file}/{input_file[i]}", "--two", f"{file}/{input_file[i+1]}"], capture_output=True, text=True)
+            ["python", "generate_out_flow.py", "--model", "default", "--one", f"{image_directory}/{input_file[i]}", "--two", f"{image_directory}/{input_file[i+1]}"], capture_output=True, text=True)
         if result_flo.stderr:
             print(f"Error while creating flow between {input_file[i]} and {input_file[i+1]}")
             print(result_flo.stderr)
